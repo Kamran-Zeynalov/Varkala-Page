a23923ab8d90b1f2ac21f0a6685c35b891fbd3e1
Kamran Z <kamraneze@code.edu.az>
2023-11-19 02:14:39 +0400

    Project is done but not responsive

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -1,143 +1,112 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let basketStr = localStorage.getItem("basket");
-  let basket = JSON.parse(basketStr);
-  if (!basket || !basket.length) {
-    localStorage.setItem("basket", JSON.stringify([]));
+  let myitemstr = localStorage.getItem("myitem");
+  let myitem = JSON.parse(myitemstr);
+
+  if (!myitem || !myitem.length) {
+    localStorage.setItem("myitem", JSON.stringify([]));
   } else {
-    showProductCount(basket);
-    showTotalPrice(basket);
-    basket.forEach((product) => {
-      AddToBasket(product);
-    });
+    ShowProductCount(myitem);
+    ShowTotalPrice(myitem);
+    ShowCartItems(myitem)
   }
 });
 
-let addToCart = document.querySelectorAll(".addToCart");
-addToCart.forEach((button) => {
-  button.addEventListener("click", function () {
-    let basket = JSON.parse(localStorage.getItem("basket"));
-    if (!basket || !basket.length) {
-      localStorage.setItem("basket", JSON.stringify([]));
-      basket = JSON.parse(localStorage.getItem("basket"));
-    }
+let buttons = document.querySelectorAll(".cart p");
 
-    let product = getProductDatas(this);
-    let existedProduct = basket.find((pro) => {
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    let myitem = JSON.parse(localStorage.getItem("myitem"));
+    if (!myitem) {
+      localStorage.setItem("myitem", JSON.stringify([]));
+      myitem = JSON.parse(localStorage.getItem("myitem"));
+    }
+    let product = GetProductsData(this);
+    let sameid = myitem.find((pro) => {
       return pro.id == product.id;
     });
-    if (!existedProduct) {
-      basket.push(product);
+    if (!sameid) {
+      myitem.push(product);
     } else {
-      existedProduct.count++;
+      sameid.count++;
     }
-    showTotalPrice(basket);
-    showProductCount(basket);
-    AddToBasket(product);
-    let basketStr = JSON.stringify(basket);
-    localStorage.setItem("basket", basketStr);
+    ShowTotalPrice(myitem);
+    ShowProductCount(myitem);
+
+    let myitemstr = JSON.stringify(myitem);
+    localStorage.setItem("myitem", myitemstr);
   });
 });
 
-function getProductDatas(product) {
-  let parent = product.parentElement.parentElement;
-  let id = parent.getAttribute("data-id");
-  let price = parent.querySelector("span.price").innerText;
-  let title = parent.querySelector(".productName h5").innerText;
+function GetProductsData(product) {
+  let parent = product.parentElement.parentElement.parentElement.parentElement.parentElement;
+  let title = parent.querySelector(".title h6").innerText;
   let src = parent.querySelector("img").src;
-  let result = { id, price, title, src, count: 1 };
+  let id = parent.getAttribute("data-id");
+  let price = parent.querySelector(".info .price .pricee").innerText;
+  result = { title, src, id, price, count: 1 };
   return result;
 }
 
-function showProductCount(basket) {
-  let basketCount = document.querySelectorAll(".wishlist");
-  basketCount.forEach((bass) => {
-    bass.innerText = basket.reduce((total, product) => {
-      return (total += product.count);
-    }, 0);
+let cart = document.querySelectorAll(".cart");
+let ul = document.querySelectorAll(".jsparent .modal-body");
+cart.forEach((crt) => {
+  crt.addEventListener("click", function () {
+    let myitem = JSON.parse(localStorage.getItem("myitem"));
+    ShowCartItems(myitem);
   });
-}
+});
 
-function showTotalPrice(basket) {
-  let total = document.querySelectorAll(".num .total");
-  total.forEach((tt) => {
-    tt.innerText = basket.reduce((total, product) => {
-      return (total += parseInt(product.price) * product.count);
-    }, 0);
-  });
-  return basket.reduce((total, product) => {
-    return (total += parseInt(product.price * product.count));
-  }, 0);
-}
 
-function AddToBasket(product) {
-  let basketDrones = document.querySelector(".basketDrones");
-  let count = document.createElement("span");
-  count.classList.add("count");
-  let existingItem = basketDrones.querySelector(`[data-id="${product.id}"]`);
-  if (existingItem) {
-    let count = existingItem.querySelector(".count");
-    count.innerText = parseInt(count.innerText) + 1;
-    return;
-  }
-
-  let shopping = document.querySelector(".noShopping");
-  let proName = document.createElement("h5");
-  let div = document.createElement("div");
-  let imgDiv = document.createElement("div");
-  let titleDiv = document.createElement("div");
-  let buttonDiv = document.createElement("div");
-  let image = document.createElement("img");
-  let price = document.createElement("p");
-  let closeBtn = document.createElement("button");
-
-  proName.innerText = product.title;
-  closeBtn.innerHTML = "<i class='fa-solid fa-trash'></i>";
-  closeBtn.style.backgroundColor = "white";
-  closeBtn.style.border = "none";
-  closeBtn.style.position = "absolute";
-  closeBtn.style.right = "16px";
-  div.className = "basket-item col-lg-12";
-  div.style.margin = "7px 6px";
-  div.style.width = "100%";
-  div.style.padding = "10px 0";
-  div.style.display = "flex";
-  div.style.position = "relative";
-  div.style.borderBottom = "1px solid lightgray";
-
-  div.style.top = "0";
-  div.setAttribute("data-id", product.id);
-  image.src = product.src;
-  count.innerText = product.count;
-  image.style.width = "100px";
-  price.innerText = product.price;
-  count.style.margin = "0 3px";
-  proName.style.margin = "0 3px";
-  proName.style.display = "inline-block";
-  titleDiv.style.margin = "0 6px";
-
-  imgDiv.append(image);
-  titleDiv.append(count, "x", proName, price);
-  buttonDiv.append(closeBtn);
-  div.append(imgDiv, titleDiv, buttonDiv);
-  basketDrones.append(div);
-
-  closeBtn.addEventListener("click", function () {
-    let basket = JSON.parse(localStorage.getItem("basket"));
-    if (!basket) {
-      localStorage.setItem("basket", JSON.stringify([]));
-      basket = JSON.parse(localStorage.getItem("basket"));
-    }
-
-    let id = this.parentElement.parentElement.getAttribute("data-id");
-    let index = basket.findIndex((element) => {
-      return element.id == id;
+function ShowCartItems(myitem) {
+  let ul = document.querySelectorAll('.jsparent .modal-body'); 
+  ul.forEach((ll) => {
+    ll.innerHTML = "";
+    myitem.forEach((item) => {
+      let task = `
+      <div class="products" id="${item.id}">
+      <div class="image">
+          <img src="${item.src}" alt="">
+      </div>
+      <div class="name-price">
+          <p class="name">${item.title}</p>
+          <p class="quantity">Quantity: <span class="baseCount">${item.count}</span></p>
+          <p class="name" style="font-family: monospace;">$${item.price}</p>
+      </div>
+      <div class="xBtn btn-close del_btn"></div>
+    </div>
+      `;
+      ll.innerHTML += task;
     });
-    delete basket[index];
-    basket = basket.filter(Object);
-    showProductCount(basket);
-    showTotalPrice(basket);
-    let basketStr = JSON.stringify(basket);
-    localStorage.setItem("basket", basketStr);
-    this.parentElement.parentElement.remove();
+  });
+  let delbtn = document.querySelectorAll(".del_btn");
+
+  delbtn.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      let li = this.parentElement;
+      let id = li.getAttribute("id");
+      myitem = myitem.filter((dev) => dev.id != id);
+      li.remove();
+      ShowTotalPrice(myitem);
+      ShowProductCount(myitem);
+      localStorage.setItem("myitem", JSON.stringify(myitem));
+    });
+  });
+}
+function ShowTotalPrice(myitem) {
+  let totals = document.querySelectorAll(".total_price");
+
+  totals.forEach((tt) => {
+    tt.innerText = myitem.reduce((total, product) => {
+      return Math.trunc((total += parseInt(product.price) * parseInt(product.count)));
+    }, 0);
+  });
+}
+
+function ShowProductCount(myitem) {
+  let myitemCount = document.querySelectorAll(".baskett");
+  myitemCount.forEach((dvc) => {
+    dvc.innerText = myitem.reduce((total, product) => {
+      return Math.trunc((total += product.count));
+    }, 0);
   });
 }
